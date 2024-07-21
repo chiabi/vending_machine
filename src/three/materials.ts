93c3753ecd371fa68c3d959367783efb1406846c
chichi <chiabi88@gmail.com>
2024-07-21 17:29:57 +0900

    feat: 글라스등 머테리얼 추가

--- src/three/materials.ts
+++ src/three/materials.ts
@@ -1,12 +1,17 @@
-import { Mesh, MeshStandardMaterial } from 'three';
+import {
+  DoubleSide,
+  Mesh,
+  MeshPhysicalMaterial,
+  MeshToonMaterial,
+} from 'three';
 
 export const applyMaterials = (mesh: Mesh): void => {
-  if (mesh.name.includes('Camera')) console.log(mesh.name);
+  if (!/Floor|Rock|Mine|Cactus|Camera|Glass/gi.test(mesh.name))
+    console.log(mesh.name);
   switch (mesh.name) {
     case 'Floor':
-      mesh.material = new MeshStandardMaterial({
-        color: 0xffdf0f,
-        roughness: 0.5,
+      mesh.material = new MeshToonMaterial({
+        color: 0xfcd783,
       });
       mesh.castShadow = true;
       mesh.receiveShadow = true;
@@ -14,14 +19,14 @@ export const applyMaterials = (mesh: Mesh): void => {
     case 'Rock1':
     case 'Rock2':
     case 'Rock3':
-      mesh.material = new MeshStandardMaterial({
+      mesh.material = new MeshToonMaterial({
         color: 0xa66d5e,
       });
       mesh.castShadow = true;
       mesh.receiveShadow = true;
       break;
     case 'Mine':
-      mesh.material = new MeshStandardMaterial({
+      mesh.material = new MeshToonMaterial({
         color: 0xffb900,
       });
       mesh.castShadow = true;
@@ -31,7 +36,7 @@ export const applyMaterials = (mesh: Mesh): void => {
     case 'Cactus2':
     case 'Cactus3':
     case 'Cactus4':
-      mesh.material = new MeshStandardMaterial({
+      mesh.material = new MeshToonMaterial({
         color: 0x04d415,
       });
       mesh.castShadow = true;
@@ -39,14 +44,14 @@ export const applyMaterials = (mesh: Mesh): void => {
       break;
     case 'MiniCactus1':
     case 'MiniCactus2':
-      mesh.material = new MeshStandardMaterial({
+      mesh.material = new MeshToonMaterial({
         color: 0x96ff45,
       });
       mesh.castShadow = true;
       mesh.receiveShadow = true;
       break;
     case 'CactusFlowers':
-      mesh.material = new MeshStandardMaterial({
+      mesh.material = new MeshToonMaterial({
         color: 0xfa5eaf,
       });
       mesh.castShadow = true;
@@ -56,7 +61,7 @@ export const applyMaterials = (mesh: Mesh): void => {
     case 'CameraBodyBase':
     case 'CameraBody':
     case 'CameraBodyLink':
-      mesh.material = new MeshStandardMaterial({
+      mesh.material = new MeshToonMaterial({
         color: 0xff72af,
       });
       mesh.castShadow = true;
@@ -64,20 +69,59 @@ export const applyMaterials = (mesh: Mesh): void => {
       break;
     case 'CameraBodyBaseScrew':
     case 'CameraStick':
-      mesh.material = new MeshStandardMaterial({
+      mesh.material = new MeshToonMaterial({
         color: 0xff9c11,
       });
       mesh.castShadow = true;
       mesh.receiveShadow = true;
       break;
     case 'CameraLight':
-      mesh.material = new MeshStandardMaterial({
+      mesh.material = new MeshToonMaterial({
         color: 0xff0000,
       });
       break;
     case 'CameraLens':
-      mesh.material = new MeshStandardMaterial({
+      mesh.material = new MeshToonMaterial({
         color: 0x302525,
+      });
+      break;
+    case 'Boolean_1':
+    case 'VendingMachineBodyCover':
+      mesh.material = new MeshToonMaterial({
+        color: 0xff0000,
+      });
+      mesh.castShadow = true;
+      mesh.receiveShadow = true;
+      break;
+    case 'Glass':
+      mesh.material = new MeshPhysicalMaterial({
+        color: 0xc1eceb,
+        metalness: 0,
+        roughness: 0,
+        transmission: 0.9,
+        transparent: true,
+        opacity: 0.5,
+        envMapIntensity: 1,
+        clearcoat: 1,
+        clearcoatRoughness: 0,
+        reflectivity: 1,
+        side: DoubleSide,
+      });
+      break;
+    case 'CanPushGlass':
+    case 'CoinPushGlass':
+      mesh.material = new MeshPhysicalMaterial({
+        color: 0xc1eceb,
+        metalness: 0,
+        roughness: 0,
+        transmission: 0.5,
+        transparent: true,
+        opacity: 0.5,
+        envMapIntensity: 1,
+        clearcoat: 1,
+        clearcoatRoughness: 0,
+        reflectivity: 1,
+        side: DoubleSide,
       });
       break;
     default:
--- src/three/scene.ts
+++ src/three/scene.ts
@@ -2,6 +2,6 @@ import { Scene, Color } from 'three';
 
 export const createScene = () => {
   const scene = new Scene();
-  scene.background = new Color(0xffdf0f);
+  scene.background = new Color(0xfcd783);
   return scene;
 };
