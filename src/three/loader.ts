1601f1dadf9c480a4451d89407deb49f4a6fbb73
chichi <chiabi88@gmail.com>
2024-07-25 01:19:16 +0900

    refactor: loader 관심사 분리

--- src/three/loader.ts
+++ src/three/loader.ts
@@ -1,15 +1,13 @@
-import { LoadingManager, Mesh, Scene } from 'three';
+import { Group, LoadingManager, Mesh, Scene } from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { applyMaterials } from './materials';
 import cans from '../assets/cans.json';
 import { createCanPriceTextMesh } from './meshes';
 
-export const loadModel = (
-  scene: Scene,
-  url: string,
+const createLoadingManager = (
   onProgress?: (progress: number) => void,
   onLoad?: () => void
-): void => {
+): LoadingManager => {
   const manager = new LoadingManager();
   manager.onProgress = (_url, itemsLoaded, itemsTotal) => {
     onProgress?.((itemsLoaded / itemsTotal) * 100);
@@ -20,23 +18,57 @@ export const loadModel = (
   manager.onError = (error) => {
     console.error('An error happened', error);
   };
+  return manager;
+};
 
+export const loadGLTF = async (
+  url: string,
+  manager: LoadingManager
+): Promise<Group> => {
   const loader = new GLTFLoader(manager);
-  loader.load(url, async (gltf) => {
-    gltf.scene.traverse((child) => {
-      if (!(child instanceof Mesh)) {
-        return;
-      }
-      applyMaterials(child);
-    });
-    scene.add(gltf.scene);
-    Object.values(cans).map(async ({ button, price }) => {
-      const mesh = gltf.scene.getObjectByName(button);
-      if (!mesh) {
-        return;
-      }
-      const textMesh = await createCanPriceTextMesh(price);
-      mesh.add(textMesh);
-    });
+  return new Promise((resolve, reject) => {
+    loader.load(
+      url,
+      (gltf) => resolve(gltf.scene),
+      undefined,
+      (error) => reject(error)
+    );
   });
+};
+
+export const processModel = (model: Group) => {
+  model.traverse((child) => {
+    if (child instanceof Mesh) {
+      applyMaterials(child);
+    }
+  });
+};
+
+export const createPriceTexts = async (model: Group) => {
+  for (const { button, price } of Object.values(cans)) {
+    const mesh = model.getObjectByName(button);
+    if (!mesh) {
+      break;
+    }
+    const textMesh = await createCanPriceTextMesh(price);
+    mesh.add(textMesh);
+  }
+};
+
+export const loadModel = async (
+  scene: Scene,
+  url: string,
+  onProgress?: (progress: number) => void,
+  onLoad?: () => void
+) => {
+  const loadingManager = createLoadingManager(onProgress, onLoad);
+
+  try {
+    const model = await loadGLTF(url, loadingManager);
+    processModel(model);
+    await createPriceTexts(model);
+    scene.add(model);
+  } catch (error) {
+    console.error('Failed to load model:', error);
+  }
 };
